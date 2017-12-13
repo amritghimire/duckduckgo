b5a6a538e3471df43099e250f8fd826e2cb4bcaf
Amrit Ghimire <iamritghimire@gmail.com>
2017-12-13 22:58:47 +0545

    Final alpha

--- d.py
+++ d.py
@@ -1,4 +1,5 @@
-import logging
+#!/usr/bin/python
+
 import signal
 import subprocess
 import sys
@@ -72,43 +73,58 @@ def strip_html(data):
 
 def search_for(search_term):
     base_url = "https://duckduckgo.com/html/?q="
-    urls = base_url + quote_plus(search_term) + "&kp=-2"
-    html = urlopen(urls).read()
-    soup = BeautifulSoup(html, "lxml")
-    results = soup.find("div", "results")
-    lists_of_results = list()
-    for result in results.find_all("div", "result"):
-        title = strip_html(str(result.find("h2", "result__title")))
-        url = "https://duckduckgo.com" + result.find('h2', 'result__title').a["href"]
-        description = strip_html(str(result.find("a", "result__snippet")))
-        lists_of_results.append(Link(url, title, description))
-    q = ''
-    start = 0
-    while q != 'q':
-        subprocess.call('clear')
-        # print(lists_of_results, len(lists_of_results))
-        for i in range(start, min(start + 10, len(lists_of_results))):
-            print(colored(":" + str(i) + ":", "red"), colored(lists_of_results[i].title, "blue"),
-                  lists_of_results[i].description)
-        if start == 0:
-            q = input("Enter the link index to open or q to exit (n=> next):")
-        elif start == 10:
-            q = input("Enter the link index to open or q to exit (p=> previous):")
-        if q.lower() == "q":
-            break
-        elif q.lower() == 'n' and start == 0:
-            start = 10
-        elif q.lower() == 'p' and start == 10:
-            start = 0
-        else:
-            try:
-                n = int(q)
+    continues = 0
+    while continues == 0:
+        urls = base_url + quote_plus(search_term.strip()) + "&kp=-2"
+        html = urlopen(urls).read()
+        soup = BeautifulSoup(html, "lxml")
+        results = soup.find("div", "results")
+        lists_of_results = list()
+        for result in results.find_all("div", "result"):
+            title = strip_html(str(result.find("h2", "result__title")))
+            url = "https://duckduckgo.com" + result.find('h2', 'result__title').a["href"]
+            description = strip_html(str(result.find("a", "result__snippet")))
+            lists_of_results.append(Link(url, title, description))
+        q = ''
+        start = 0
+        zci = soup.find("div", "zci__result")
+        zci_text = ""
+        if zci:
+            zci_text = strip_html(str(zci))
+        while q != 'q':
+            subprocess.call('clear')
+            # print(lists_of_results, len(lists_of_results))
+            if zci:
+                print(colored(zci_text, "blue"))
+            for i in range(start, min(start + 10, len(lists_of_results))):
+                print(colored(":" + str(i) + ":", "red"), colored(lists_of_results[i].title, "blue"),
+                      lists_of_results[i].description)
+            if start == 0:
+                q = input("Enter the link index to open or q to exit (n=> next):")
+            elif start == 10:
+                q = input("Enter the link index to open or q to exit (p=> previous):")
+            if q.lower() == "q":
+                break
+            elif q.lower() == 'n' and start == 0:
+                start = 10
+            elif q.lower() == 'p' and start == 10:
+                start = 0
+            elif q[0] == ':':
+                search_term = q[0:]
+                q = 'q'
+                continues -= 1
+            else:
+                try:
+                    n = int(q)
 
-                if n < len(lists_of_results):
-                    url_open(lists_of_results[n].url)
-            except ValueError:
-                pass
+                    if n < len(lists_of_results):
+                        url_open(lists_of_results[n].url)
+                except ValueError:
+                    pass
+        continues += 1
 
 
-search_text = input("Enter search text: ")
+search_text = ' '.join(sys.argv[1:])
+if not search_text:
+    search_text = input("Enter search text: ")
 search_for(search_text)
