00526cceec5a4f951768b94270f789d7f0ee8383
Amrit Ghimire <iamritghimire@gmail.com>
2017-12-14 19:00:56 +0545

    Started working for config

--- d.py
+++ d.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import signal
 import subprocess
 import sys
 from termcolor import colored
 import re
-
+import configparser
 import os
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -38,20 +38,10 @@ signal.signal(signal.SIGINT, sigint_handler)
 
 _VERSION_ = '3.4'
 
-COLORMAP = {k: '\x1b[%sm' % v for k, v in {
-    'a': '30', 'b': '31', 'c': '32', 'd': '33',
-    'e': '34', 'f': '35', 'g': '36', 'h': '37',
-    'i': '90', 'j': '91', 'k': '92', 'l': '93',
-    'm': '94', 'n': '95', 'o': '96', 'p': '97',
-    'A': '30;1', 'B': '31;1', 'C': '32;1', 'D': '33;1',
-    'E': '34;1', 'F': '35;1', 'G': '36;1', 'H': '37;1',
-    'I': '90;1', 'J': '91;1', 'K': '92;1', 'L': '93;1',
-    'M': '94;1', 'N': '95;1', 'O': '96;1', 'P': '97;1',
-    'x': '0', 'X': '1', 'y': '7', 'Y': '7;1',
-}.items()}
+configParser=configparser.RawConfigParser()
+configFilePath=r'core.conf'
+configParser.read(configFilePath)
 
-# Disguise as Firefox on Ubuntu
-USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:56.0) Gecko/20100101 Firefox/56.0'
 
 
 def url_open(url):
